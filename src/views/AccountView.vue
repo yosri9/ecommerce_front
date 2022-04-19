503d282e9e4f43c0f505871a1ec576c77251ede8
yosri <abdedayemysori@gmail.com>
2022-04-19 10:37:54 +0000

    update account page

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <v-col class="pt-16" >
-    <v-row class="mb-10" >
+  <v-col class="pt-16">
+    <v-row class="mb-10">
 
       <v-col cols="2"/>
       <v-col cols="2">
@@ -68,16 +68,17 @@
 
       </v-col>
       <v-col
+          class="d-flex"
           cols="12"
-          sm="6"
-          md="4"
+          sm="4"
       >
-        <v-text-field
-            outlined
+        <v-select
+            :items="items"
+            label="Select"
             dense
-        ></v-text-field>
+            solo
+        ></v-select>
       </v-col>
-
 
     </v-row>
     <v-row class="align-baseline">
@@ -112,11 +113,36 @@
           sm="6"
           md="4"
       >
-        <v-text-field
-            outlined
-            dense
-        ></v-text-field>
+
+        <v-menu
+            ref="menu1"
+            v-model="menu1"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+                v-model="dateFormatted"
+                label="Date"
+                hint="MM/DD/YYYY format"
+                persistent-hint
+                prepend-icon="mdi-calendar"
+                v-bind="attrs"
+                @blur="date = parseDate(dateFormatted)"
+                v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+              v-model="date"
+              no-title
+              @input="menu1 = false"
+          ></v-date-picker>
+        </v-menu>
       </v-col>
+
 
 
     </v-row>
@@ -128,11 +154,13 @@
 
       </v-col>
       <v-col
+
           cols="12"
           sm="6"
           md="4"
       >
         <v-text-field
+            type="password"
             outlined
             dense
         ></v-text-field>
@@ -164,8 +192,58 @@
 
 </template>
 <script>
+
+
+
+
 export default {
-  name: "AccountView"
+  data: () => ({
+
+  }),
+}
+</script>
+
+
+
+<script>
+export default {
+  data: vm => ({
+    date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    dateFormatted: vm.formatDate((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)),
+    menu1: false,
+    menu2: false,
+    name: "AccountView",
+    items: ['male', 'female'],
+
+
+  }),
+
+  computed: {
+    computedDateFormatted () {
+      return this.formatDate(this.date)
+    },
+  },
+
+  watch: {
+    date (val) {
+      this.dateFormatted = this.formatDate(this.date)
+    },
+  },
+
+  methods: {
+    formatDate (date) {
+      if (!date) return null
+
+      const [year, month, day] = date.split('-')
+      return `${month}/${day}/${year}`
+    },
+    parseDate (date) {
+      if (!date) return null
+
+      const [month, day, year] = date.split('/')
+      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+    },
+  },
 }
 </script>
 
--- src/views/PaymentInformationView.vue
+++ src/views/PaymentInformationView.vue
@@ -1,6 +1,139 @@
 <template>
-<div></div>
+<div>
+
+
+  <v-col>
+<v-row class="pt-16 " style="padding-left: 15%; text-decoration: underline ; text-underline-position: under">
+  Payment Information
+</v-row>
+    <br/>
+    <br/>
+
+    <v-row class="align-baseline justify-content-center " >
+      <v-col cols="2"/>
+      <v-col cols="2">
+        <div class="font-weight-bold">Card Credit Number:</div>
+
+
+      </v-col>
+      <v-col
+          cols="12"
+          sm="6"
+          md="4"
+
+      >
+        <v-text-field
+            outlined
+            dense
+            class="mt-15"
+        ></v-text-field>
+      </v-col>
+      <v-spacer/>
+      <v-col cols="2">
+        <v-btn
+            class="ma-2 center mb-3"
+            :loading="loading2"
+            :disabled="loading2"
+            color="success"
+            @click="loader = 'loading2'"
+        >
+          update
+          <template v-slot:loader>
+            <span>Loading...</span>
+          </template>
+        </v-btn>
+
+      </v-col>
+
+
+    </v-row>
+
+    <v-row class="align-baseline" style="alignment-baseline: center">
+      <v-col cols="2"/>
+      <v-col cols="2">
+        <div class="font-weight-bold">Billing Address:</div>
+
+
+      </v-col>
+      <v-col
+          cols="12"
+          sm="6"
+          md="4"
+      >
+        <v-textarea
+            rows="3"
+            outlined
+            dense
+        ></v-textarea>
+      </v-col>
+      <v-spacer/>
+
+      <v-col cols="2">
+        <v-btn
+            class="ma-2 center mt-6"
+            :loading="loading2"
+            :disabled="loading2"
+            color="success"
+            @click="loader = 'loading2'"
+        >
+          update
+          <template v-slot:loader>
+            <span>Loading...</span>
+          </template>
+        </v-btn>
+
+      </v-col>
+
+
+    </v-row>
+
+
+    <v-row class="align-baseline">
+      <v-col cols="2"/>
+      <v-col cols="2">
+        <div class="font-weight-bold">Shipping Address:</div>
+
+
+      </v-col>
+      <v-col
+          cols="12"
+          sm="6"
+          md="4"
+      >
+        <v-textarea
+            rows="3"
+            outlined
+            dense
+        ></v-textarea>
+      </v-col>
+      <v-spacer/>
+
+      <v-col cols="2">
+        <v-btn
+            class="ma-2 center  mt-6"
+            :loading="loading2"
+            :disabled="loading2"
+            color="success"
+            @click="loader = 'loading2'"
+        >
+          update
+          <template v-slot:loader>
+            <span>Loading...</span>
+          </template>
+        </v-btn>
+
+      </v-col>
+
+
+    </v-row>
+
+
+  </v-col>
+
+</div>
 </template>
+
+
 
 <script>
 export default {
